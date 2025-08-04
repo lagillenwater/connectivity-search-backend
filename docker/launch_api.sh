e3624c1ae74b60c6f10b98524672d0436329f1b8
Faisal Alquaddoomi <faisal.alquaddoomi@cuanschutz.edu>
2025-08-04 17:04:19 -0600

    Adds Dockerfile for API, launch script, templates YAML built from env vars

---
+++ docker/launch_api.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# create secrets.yml from env vars via envsubst
+envsubst \
+    < /app/dj_hetmech/secrets.yml.template \
+    > /app/dj_hetmech/secrets.yml
+
+gunicorn dj_hetmech.wsgi:application --bind 0.0.0.0:8001 --workers=${WEB_WORKERS:-3}
